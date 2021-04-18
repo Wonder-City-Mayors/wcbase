ee255afe851365f43e821f24ee4921ea88afd9a7
Kimp13 <akunec41@gmail.com>
2021-04-18 21:35:21 +0400

    🔮 Истоки великого и непостижимого

---
+++ src/wcbase.cpp
@@ -0,0 +1,36 @@
+﻿#include "wcbase.hpp"
+
+typedef websocketpp::client<websocketpp::config::asio_client> socket_client;
+
+auto check_dotenv() {
+    std::ifstream dotenv("./.env.json");
+
+    if (dotenv.fail()) {
+        throw std::runtime_error(".env.json file was not found.");
+    }
+
+    auto env = std::make_unique<nlohmann::json>();
+
+    dotenv >> *env;
+
+    return env;
+}
+
+int main(int argc, char* argv[]) {
+#if defined(WIN32) || defined(_WIN32) || \
+    defined(__WIN32) && !defined(__CYGWIN__)
+    SetConsoleOutputCP(CP_UTF8);
+#endif
+
+    std::unique_ptr<nlohmann::json> env;
+
+    try {
+        env = std::unique_ptr<nlohmann::json>(check_dotenv());
+    } catch (std::exception& e) { std::cerr << e.what() << '\n'; }
+
+    /*
+     * А чё дальше-то?
+     */
+
+    return 0;
+}
---
+++ src/wcbase.hpp
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include "nlohmann/json.hpp"
+#include "websocketpp/client.hpp"
+#include "websocketpp/config/asio_no_tls_client.hpp"
+
+#include <fstream>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+
+#if defined(WIN32) || defined(_WIN32) || \
+    defined(__WIN32) && !defined(__CYGWIN__)
+    #include <Windows.h>
+#endif
